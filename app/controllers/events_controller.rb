3c803b1f8375b10526d27570baa9c2d48b8e48ba
enoch232 <enoch232@gmail.com>
2016-03-29 01:27:16 -0700

    correct navigation, and complete CRUD for event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,16 +1,31 @@
 class EventsController < ApplicationController
+	before_action :event_find , only: [:destroy , :edit, :update]
 	def index
 		@events = Event.all.order("created_at DESC")
 	end
 	def new 
+		@event = Event.new
 	end
 	def create
+		@event = Event.new(event_params)
+		if @event.save
+			redirect_to root_path
+		else
+			render :new
+		end
 	end
 	def edit
 	end
 	def update
+		if @event.update(event_params)
+			redirect_to events_path
+		else
+			render :edit
+		end
 	end
 	def destroy
+		@event.destroy
+		redirect_to events_path
 	end
 	private
 	def event_find
