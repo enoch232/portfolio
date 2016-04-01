8a75b259166dd69ec94f4114c497028c1593f0aa
enoch232 <enoch232@gmail.com>
2016-04-01 10:18:29 -0700

    fix footer problem

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-	has_attached_file :photo, styles: { thumb: "1000x1000#"}, default_url: "/images/:style/missing.png"
+	has_attached_file :photo, styles: { thumb: "100x100#"}, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
