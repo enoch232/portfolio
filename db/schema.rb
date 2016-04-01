57658eb4754bb95b60632e31fa8e4182926959a8
enoch232 <enoch232@gmail.com>
2016-04-01 02:42:31 -0700

    project index design change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329080134) do
+ActiveRecord::Schema.define(version: 20160401083812) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -28,11 +28,15 @@ ActiveRecord::Schema.define(version: 20160329080134) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "title"
     t.text     "description"
     t.string   "url"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
